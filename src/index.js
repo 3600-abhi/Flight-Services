8ab4bff5f13d34f7933d51cb851e5d5cd259c6a2
3600-abhi <abhishekjaiswal3600@gmail.com>
2023-06-25 19:24:09 +0530

    little changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-const express = require("express");
-const { ServerConfig } = require("./config");
-const apiRoutes = require("./routes");
+const express = require('express');
+const { ServerConfig } = require('./config');
+const apiRoutes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", apiRoutes);
+app.use('/api', apiRoutes);
+
+// If the request from Reverse-Proxy Server
+// app.use('/flightservice/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started the server at PORT : ${ServerConfig.PORT}`);
